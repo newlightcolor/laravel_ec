794d30b47c68c4f21452ff609cf2280c96608131
newlightcolor <newlightcolor@gmail.com>
2023-06-11 17:04:06 +0900

    update orderスキーマ修正

--- database/migrations/2023_06_10_155731_create_orders_table.php
+++ database/migrations/2023_06_10_155731_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('customer_id');
             $table->timestamps();
         });
     }
