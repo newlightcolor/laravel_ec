8ba59298f4245705aa7bee5b5329c3e675fef476
newlightcolor <newlightcolor@gmail.com>
2023-06-14 01:41:48 +0900

    update インストール手順にスキーマ設定を追記

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ sh installation.sh
 **やっていること**
 - envファイル作成
 - composer update
+- Migration実行
+- Seeder実行
 
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
--- installation.sh
+++ installation.sh
@@ -4,4 +4,6 @@ PROJECT_DIR=`echo ${PROJECT_PATH##*/}`
 
 cp $PROJECT_PATH/.env.local $PROJECT_PATH/.env
 docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && composer update"
+docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && php artisan migrate"
+docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && php artisan db:seed"
 # sudo chmod 777 -R ${PROJECT_PATH}
